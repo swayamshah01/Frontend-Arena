7b7f337f4ee4264e3f075168cf719dd2b041131e
mehtatanay15 <mehtatanay04@gmail.com>
2025-03-24 21:41:23 +0530

    HomePage Progress

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
+import VisionSection from '../components/VisionSection'
+import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center flex-nowrap flex-col'>
       <HeroSection />
+      <AboutSection />
+      <VisionSection />
+      <Footer />
     </div>
   )
 }
