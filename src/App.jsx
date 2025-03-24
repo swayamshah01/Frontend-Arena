c3b683c8556cfaad5acdde643feddf7bad061b72
swayamshah01 <sswayam351@gmail.com>
2025-03-24 15:09:22 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='h-36 w-full bg-black text-white'>Hellow World</h1>
+    </div>
+  )
+}
+
+export default App
