2b26aa81b42d4f4915fd6d0d7eecd9bb8747d954
tanishshah <shahtanish207@gmail.com>
2025-03-24 20:38:49 +0530

    File Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-
-
-import MuseumLandingPage2 from './Pages/MuseumLandingPage2';
+import { Routes,Route } from 'react-router-dom'
+import Gallery from './Pages/Gallery';
+import Header from './Header';
+import Footer from './Footer';
+import NotFound from './Pages/NotFound';
+import HomePage from './Pages/HomePage';
 
 const App = () => {
   return (
-    <div>
-      <MuseumLandingPage2/>
+    <div className='min-h-screen max-w-screen m-0 p-0 overflow-x-hidden overflow-y-auto relative transition-all scroll-smooth'>
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/gallery" element={<Gallery />} />
+  
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      
+      <Footer />
     </div>
   )
 }
